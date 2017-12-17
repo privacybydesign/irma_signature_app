76ea52c9c2c9f51a4206a8b70d951102b1e6a0bd
Confiks <confiks@scriptbase.org>
2017-12-18 00:48:29 +0100

    Organization renames in submodule urls and Go import paths

--- app/utils/irma_cli_search/main.go
+++ app/utils/irma_cli_search/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
   "fmt"
-  "github.com/credentials/irmago"
+  "github.com/privacybydesign/irmago"
   "encoding/json"
   "strings"
   "os"
--- app/utils/irmago_bridge/main.go
+++ app/utils/irmago_bridge/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/credentials/irmago"
+	"github.com/privacybydesign/irmago"
 	"github.com/gopherjs/gopherjs/js"
 	"strings"
 )
