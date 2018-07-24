355525493db5fafbb0c869e359ca9affc73b953c
David Venhoek <david@venhoek.nl>
2018-07-24 11:23:51 +0200

    Changed attributechip to show more of the attributes image.

--- src/containers/AttributeChip.js
+++ src/containers/AttributeChip.js
@@ -2,18 +2,28 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Chip from '@material-ui/core/Chip';
 import Avatar from '@material-ui/core/Avatar';
+import { withStyles } from '@material-ui/core/styles';
 
 import 'react-select/dist/react-select.css';
 
+const styles = theme => ({
+  img: {
+    width: '75%',
+    height: '75%',
+    textAlign: 'center',
+    objectFit: 'cover',
+  },
+});
+
 class AttributeChip extends Component {
   render() {
-    const { onDelete, attribute } = this.props;
+    const { onDelete, attribute, classes } = this.props;
     const logoUrl = attribute.logo.substr(0, 3) === 'go/' ? attribute.logo.substr(3) : attribute.logo;
     return (
       <Chip
         label={`${attribute.name} - ${attribute.credentialName}`}
         onDelete={onDelete}
-        avatar={logoUrl ? <Avatar src={logoUrl} /> : null}
+        avatar={logoUrl ? <Avatar src={logoUrl} classes={{img: classes.img}}/> : null}
       />
     );
   }
@@ -22,6 +32,7 @@ class AttributeChip extends Component {
 AttributeChip.propTypes = {
   onDelete: PropTypes.func,
   attribute: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
 };
 
-export default AttributeChip;
+export default withStyles(styles, { withTheme: true })(AttributeChip);
