429de3c1082e0ed4eaa75284bbbd867600293b72
David Venhoek <david@venhoek.nl>
2018-07-31 09:13:28 +0200

    Added info box componenet.

---
+++ src/containers/LocalInfoBox.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import IconButton from '@material-ui/core/IconButton';
+import Popover from '@material-ui/core/Popover';
+import HelpIcon from '@material-ui/icons/Info';
+import Typography from '@material-ui/core/Typography';
+
+class LocalInfoBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+  
+  render() {
+    return (
+      <React.Fragment>
+        <IconButton 
+          buttonRef={(ref)=>{this.anchorEl=ref;}}
+          onClick={()=>{this.setState({open:true});}}
+          >
+          <HelpIcon/>
+        </IconButton>
+        <Popover 
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={this.state.open}
+          anchorEl={this.anchorEl}
+          onClose={()=>{this.setState({open:false});}}
+        >
+          <Typography style={{margin:'1em'}}>{this.props.text}</Typography>
+        </Popover>
+      </React.Fragment>
+    );
+  }
+}
+
+LocalInfoBox.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default LocalInfoBox
